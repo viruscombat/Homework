2fc6e50ce19ddc131fb4598e19e85755532077ef
viruscombat <constantinyrsu@gmail.com>
2023-07-06 14:01:49 +0300

    calculeaza numarulr de caractere in text

---
+++ chount_char_in_text.py
@@ -0,0 +1,17 @@
+# объявили функцию для подсчета количества символов в неком абстрактном тексте
+text = str(input())
+def char_frequency(text):
+   text = text.lower()
+   text = text.replace(" ", "")
+   text = text.replace("\n", "")
+
+   count = {}  # для подсчета символов и их количества
+   for char in text:
+       if char in count:  # если символ уже встречался, то увеличиваем его количество на 1
+           count[char] += 1
+       else:
+           count[char] = 1
+
+   for char, cnt in count.items():
+       print(f"Символ {char} встречается {cnt} раз")
+char_frequency(text)
