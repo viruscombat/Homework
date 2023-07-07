ae847c9f7314c923223b4baf1fc08a795ccc0158
viruscombat <constantinyrsu@gmail.com>
2023-07-07 09:40:53 +0300

    b inlocuit cu n

--- check_num.py
+++ check_num.py
@@ -1,9 +1,9 @@
 a =int(input())
-b = int(input())
+n = int(input())
 def check_num(a, n):
    if a % n == 0:
        print(f"Число {n} является делителем числа {a}")
    else:
        print(f"Число {n} не является делителем числа {a}")
 
-check_num(a,b)
+check_num(a,n)
